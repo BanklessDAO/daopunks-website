6951386ff47b040fc249efe9840a9b79eadf6b84
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2021-10-22 08:07:13 +0530

    updated nft images

--- src/components/home/daopunks.js
+++ src/components/home/daopunks.js
@@ -1,6 +1,12 @@
-import DaopunkAnon1 from "../../assets/images/nfts/daopunk-anon-1.png";
-import DaopunkAnon2 from "../../assets/images/nfts/daopunk-anon-2.png";
-import DaopunkAnon3 from "../../assets/images/nfts/daopunk-anon-3.png";
+import DaopunkAnon1 from "../../assets/images/nfts/1.png";
+import DaopunkAnon2 from "../../assets/images/nfts/2.png";
+import DaopunkAnon3 from "../../assets/images/nfts/3.png";
+import DaopunkAnon4 from "../../assets/images/nfts/4.png";
+import DaopunkAnon5 from "../../assets/images/nfts/5.png";
+import DaopunkAnon6 from "../../assets/images/nfts/6.png";
+import DaopunkAnon7 from "../../assets/images/nfts/7.png";
+import DaopunkAnon8 from "../../assets/images/nfts/8.png";
+import DaopunkAnon9 from "../../assets/images/nfts/9.png";
 
 export default function DaoPunks({ showMore, setShowMore }) {
   const daopunks = !showMore
@@ -9,19 +15,19 @@ export default function DaoPunks({ showMore, setShowMore }) {
         DaopunkAnon1,
         DaopunkAnon2,
         DaopunkAnon3,
-        DaopunkAnon3,
-        DaopunkAnon1,
-        DaopunkAnon2,
-        DaopunkAnon2,
-        DaopunkAnon3,
-        DaopunkAnon1,
+        DaopunkAnon4,
+        DaopunkAnon5,
+        DaopunkAnon6,
+        DaopunkAnon7,
+        DaopunkAnon8,
+        DaopunkAnon9  ,
       ];
 
   return (
     <>
       <div className="mt-10 md:mt-24 grid gap-5 lg:gap-10 grid-cols-1 md:grid-cols-3 place-items-center px-5 md:px-14">
         {daopunks.map((anon, key) => {
-          return <img key={key} src={anon} alt="" />;
+          return <img className="rounded-lg" key={key} src={anon} alt="" />;
         })}
       </div>
       <button
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  theme: {
-    maxHeight: {
-      120: "30rem"
-    },
-  },
-  variants: {},
-  plugins: [],
-};
