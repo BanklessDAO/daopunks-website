1efd662aeee7f720f4fe3ba6e53e31a9363b5f40
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2021-10-28 06:28:31 +0530

    made hero 4k compatible

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -96,16 +96,19 @@ export default function Hero() {
           </div>
           <div className="hidden xl:flex w-full h-full flex-col justify-center text-left text-white px-5 pb-10 sm:pb-20">
             <div className="md:max-w-xl">
-              <div className="modius-bold text-6xl">
+              <div className="modius-bold text-5xl">
                 We BLAZE TRAILS. <br /> WE BUILD BRIDGES.
               </div>
-              <div className="modius text-5xl">
+              <div
+                style={{ fontSize: "2.5rem", lineHeight: "2.8rem" }}
+                className="modius max-w-md"
+              >
                 A BETTER WORLD AWAITS DAOPUNKS LEAD THE WAY
               </div>
             </div>
             <a
               href="#mint"
-              className="hidden md:block w-max px-10 mt-5 py-5 rounded red text-white modius-bold text-xl leading-snug text-black outline-none cursor-pointer"
+              className="hidden md:block w-max px-8 mt-5 py-5 rounded red text-white modius-bold text-xl leading-snug text-black outline-none cursor-pointer"
             >
               MINT A DAOPUNK
             </a>
