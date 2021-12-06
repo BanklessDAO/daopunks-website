95cd0413ef2186ab3f8e6149058361e98accc64d
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2021-12-06 11:16:51 +0530

    added other dao punks section

--- src/components/nft-gallery/nft.js
+++ src/components/nft-gallery/nft.js
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 export default function NFT() {
   const [nftId, setNftId] = useState(null);
   const [metadata, setMetadata] = useState(null);
+  const [otherDAOPunks, setOtherDAOPunks] = useState(null);
 
   async function getNftId() {
     const url = window.location.pathname;
@@ -41,6 +43,23 @@ export default function NFT() {
       });
   }
 
+  async function getOtherDAOPunks() {
+    const otherDaoPunksMetadata = [];
+
+    for (let i = 0; i < 4; i++) {
+      const nftId = Math.floor(Math.random() * (1111 - 1 + 1) + 1);
+      const nftURL = `/gallery/${nftId}`;
+      const nftImage = `https://daopunks.fra1.digitaloceanspaces.com/resized/${nftId}.png`;
+
+      otherDaoPunksMetadata.push({
+        nftURL: nftURL,
+        nftImage: nftImage,
+      });
+    }
+
+    setOtherDAOPunks(otherDaoPunksMetadata);
+  }
+
   useEffect(() => {
     if (!nftId) {
       getNftId();
@@ -48,10 +67,13 @@ export default function NFT() {
     if (nftId && !metadata) {
       getMetadata();
     }
+    if (!otherDAOPunks) {
+      getOtherDAOPunks();
+    }
   });
 
   return (
-    <div className="w-screen h-full flex justify-center">
+    <div className="w-screen h-full flex flex-col items-center">
       {!metadata ? (
         <div className="h-full flex items-center justify-center">
           <svg
@@ -76,85 +98,108 @@ export default function NFT() {
           </svg>
         </div>
       ) : (
-        <div className="w-max flex flex-col lg:flex-row items-center lg:items-start lg:justify-between modius-bold lg:px-5 max-w-screen-xl">
-          <div className="w-11/12 lg:w-full mt-20">
-            <div
-              style={{ aspectRatio: "1 / 1" }}
-              className={`${
-                metadata && metadata.imageLoaded ? "hidden" : "block"
-              } w-full rounded-xl animate-pulse bg-gray-200`}
-            ></div>
-            <img
-              className={`${
-                metadata && metadata.imageLoaded ? "block" : "hidden"
-              } w-full rounded-xl`}
-              src={metadata ? metadata.image : ""}
-              onLoad={() => setMetadata({ ...metadata, imageLoaded: true })}
-            />
-          </div>
-          {metadata ? (
-            <div className="w-full flex flex-col items-center lg:items-start lg:px-5">
-              <h1 className="lg:mt-20 my-10 text-center text-3xl lg:text-5xl">
-                {metadata.name}
-              </h1>
-              <div className="px-5 lg:px-0">
-                <div>
-                  <h2 className="text-lg">DESCRIPTION</h2>
-                  <p
-                    style={{ lineHeight: "20px", color: "rgba(0, 0, 0, 0.6)" }}
-                    className="open-sans-light text-lg mt-2"
-                  >
-                    {metadata.description}
-                  </p>
-                </div>
-                <div className="mt-5">
-                  <h2 className="text-lg">PROPERTIES</h2>
-                  <div className="lg:w-max mt-5 grid grid-cols-2 lg:grid-cols-3 justify-items-start items-start gap-3">
-                    {metadata.attributes.map((attr, key) => (
-                      <div
-                        key={key}
-                        style={{
-                          background: "rgba(228, 118, 27, 0.1)",
-                          border: "1px solid rgba(228, 118, 27, 0.5)",
-                        }}
-                        className="w-full lg:w-max flex flex-col items-center justify-center gap-y-2 rounded p-3 lg:px-5  open-sans"
-                      >
-                        <h3 style={{ color: "rgba(228, 118, 27, 1)" }}>
-                          {attr.trait_type}
-                        </h3>
-                        <p
-                          style={{ color: "rgba(0, 0, 0, 0.6)" }}
-                          className="w-max text-center text-sm"
+        <>
+          <div className="w-max flex flex-col lg:flex-row items-center lg:items-start lg:justify-between lg:px-5 max-w-screen-xl  modius-bold">
+            <div className="w-11/12 lg:w-full mt-20">
+              <div
+                style={{ aspectRatio: "1 / 1" }}
+                className={`${
+                  metadata && metadata.imageLoaded ? "hidden" : "block"
+                } w-full rounded-xl animate-pulse bg-gray-200`}
+              ></div>
+              <img
+                className={`${
+                  metadata && metadata.imageLoaded ? "block" : "hidden"
+                } w-full rounded-xl`}
+                src={metadata ? metadata.image : ""}
+                onLoad={() => setMetadata({ ...metadata, imageLoaded: true })}
+              />
+            </div>
+            {metadata ? (
+              <div className="w-full flex flex-col items-center lg:items-start lg:px-5">
+                <h1 className="lg:mt-20 my-10 text-center text-3xl lg:text-5xl">
+                  {metadata.name}
+                </h1>
+                <div className="px-5 lg:px-0">
+                  <div>
+                    <h2 className="text-lg">DESCRIPTION</h2>
+                    <p
+                      style={{
+                        lineHeight: "20px",
+                        color: "rgba(0, 0, 0, 0.6)",
+                      }}
+                      className="open-sans-light text-lg mt-2"
+                    >
+                      {metadata.description}
+                    </p>
+                  </div>
+                  <div className="mt-5">
+                    <h2 className="text-lg">PROPERTIES</h2>
+                    <div className="lg:w-max mt-5 grid grid-cols-2 lg:grid-cols-3 justify-items-start items-start gap-3">
+                      {metadata.attributes.map((attr, key) => (
+                        <div
+                          key={key}
+                          style={{
+                            background: "rgba(228, 118, 27, 0.1)",
+                            border: "1px solid rgba(228, 118, 27, 0.5)",
+                          }}
+                          className="w-full lg:w-max flex flex-col items-center justify-center gap-y-2 rounded p-3 lg:px-5  open-sans"
                         >
-                          ANON
-                          <br />
-                          25% have this trait
-                        </p>
-                      </div>
-                    ))}
+                          <h3 style={{ color: "rgba(228, 118, 27, 1)" }}>
+                            {attr.trait_type}
+                          </h3>
+                          <p
+                            style={{ color: "rgba(0, 0, 0, 0.6)" }}
+                            className="w-max text-center text-sm"
+                          >
+                            ANON
+                            <br />
+                            25% have this trait
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
+                <div className="mt-8 flex flex-col lg:flex-row gap-y-5 gap-x-5">
+                  <a
+                    style={{ backgroundColor: "rgba(0, 133, 255, 0.5)" }}
+                    className="px-auto w-72 h-14 rounded-lg text-white flex items-center justify-center"
+                    href={metadata.openseaURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    SEE ON OPENSEA
+                  </a>
+                  <button
+                    style={{ backgroundColor: "rgb(255, 0, 0)" }}
+                    className="mb-20 px-auto w-72 h-14 rounded-lg text-white"
+                  >
+                    REDEEM TEE
+                  </button>
+                </div>
               </div>
-              <div className="mt-8 flex flex-col lg:flex-row gap-y-5 gap-x-5">
-                <a
-                  style={{ backgroundColor: "rgba(0, 133, 255, 0.5)" }}
-                  className="px-auto w-72 h-14 rounded-lg text-white flex items-center justify-center"
-                  href={metadata.openseaURL}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  SEE ON OPENSEA
-                </a>
-                <button
-                  style={{ backgroundColor: "rgb(255, 0, 0)" }}
-                  className="mb-20 px-auto w-72 h-14 rounded-lg text-white"
-                >
-                  REDEEM TEE
-                </button>
-              </div>
+            ) : null}
+          </div>
+          <div className="w-full flex flex-col items-center justify-center mt-36">
+            <h2 className="text-5xl text-red modius-bold">OTHER DAO PUNKS</h2>
+            <div className="mt-10 mb-52 flex gap-x-5">
+              {otherDAOPunks
+                ? otherDAOPunks.map((otherDaoPunk, key) => (
+                    <a key={key} href={otherDaoPunk.nftURL}>
+                      <img
+                        src={otherDaoPunk.nftImage}
+                        className="w-72 h-72 rounded-lg"
+                      />
+                    </a>
+                  ))
+                : null}
             </div>
-          ) : null}
-        </div>
+          </div>
+          <div className="w-full py-9 bg-black text-white text-xl flex items-center justify-center">
+            DAOPunks® - 2021
+          </div>
+        </>
       )}
     </div>
   );
